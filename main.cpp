2690c635db77f76f0cabd94128b677e1cd9c1dac
goshag45 <goshagugl44@gmail.com>
2024-06-07 20:07:20 +1000

    piglatin test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
-using namespace std;
+std::string getUserInput() {
+    std::string userInput;
+    std::cout << "Please enter string to pigify: ";
+    std::cin >> userInput;
+    std::cout << "Thank you" << std::endl;
+    return userInput;
+}
+
+std::string pigify(std::string inputString) {
+    char consArr[22] = "bcdfghjklmnpqrstvwxyz";
+    char vowelArr[6] = "aeiou";
+    return "blah";
+}
 
 int main() {
-    cout << "Hello World\n" << endl;
+    std::cout << "PIG LATIN BOX" << std::endl;
+    std::string userInput = getUserInput();
+    std::cout << pigify(userInput);
     return 0;
 }
