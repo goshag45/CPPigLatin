2c3df7179881be8e27585eedb8b42701b1cb76a8
goshag45 <goshagugl44@gmail.com>
2024-06-09 19:17:01 +1000

    logic rewrite incoming

--- main.cpp
+++ main.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
 
 std::string getUserInput() {
     std::string userInput;
     std::cout << "Please enter string to pigify: ";
     std::cin >> userInput;
-    std::cout << "Thank you" << std::endl;
+    std::cout << "Thank you for the input" << std::endl;
     return userInput;
+}
+
+// Function to check if a character is a vowel
+bool isVowel(char ch) {
+    char vowels[] = "aeiouAEIOU";
+    return strchr(vowels, ch) != nullptr;
 }
 
 // this will only work for individual words at the moment, damn it
 std::string pigify(std::string inputString) {
-    // IF CONS, MOVE TO END AND ADD AY
-    // IF VOWEL, ADD YAY
-    char vowelArr[6] = "aeiou";
-    char consArr[22] = "bcdfghjklmnpqrstvwxyz";
     std::string piggedOutput = inputString;
-    // loop through input string as characters until you reach a 
-    // if character is in vowelArr do blah
-    // if character is in consArr do blah
-    // move char to end of inputString
-    for (int i = 0; i < inputString.size(); i++) {
-        if (strchr(consArr, inputString[i])) {
-            piggedOutput = piggedOutput.substr(1, piggedOutput.length() - 1) + piggedOutput.front();
-        }
-        if (strchr(consArr, piggedOutput[0])) {
-            piggedOutput += "ay";
-            break;
-        }
-        if (strchr(consArr, inputString[0])) {
-            piggedOutput += "yay";
-            break;
-        }
-    }
+
 
     return piggedOutput;
 }
@@ -41,6 +28,6 @@ std::string pigify(std::string inputString) {
 int main() {
     std::cout << "PIG LATIN BOX" << std::endl;
     std::string userInput = getUserInput();
-    std::cout << pigify(userInput);
+    std::cout << "Pigified: " << pigify(userInput) << std::endl;
     return 0;
 }
