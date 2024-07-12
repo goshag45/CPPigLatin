ef2cb501e89ccf110e72a777d90b649dab60498d
goshag45 <goshagugl44@gmail.com>
2024-07-12 16:18:30 +1000

    planning muahahah

--- parser.cpp
+++ parser.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <stdio.h>
 
+// setup text file as json kinda?
+// go through entire list
+// pigify each word 
+// write to json file as pigifiedword:originalword
+// will have to write a json parses OH WELL
+
+std::string Pigify(const std::string &word) {
+    std::string pigified = word;
+
+    return word;
+}
+
 int main() {
-    
+
     return 0;
 }
