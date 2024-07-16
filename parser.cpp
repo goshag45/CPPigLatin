9645784642b6186d5d5d34a3461d75e498ae5573
goshag45 <goshagugl44@gmail.com>
2024-07-16 17:31:29 +1000

    brain expanding

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream> 
 #include <string>
+#include <vector>
 
 #include "functions.h"
 
@@ -10,18 +11,22 @@
 // write to json file as pigifiedword:originalword
 // will have to write a json parses OH WELL
 
-void ParseFile(std::string& fileName) {
+std::vector<std::string> ParseFile(std::string& fileName) {
     std::ifstream file(fileName);
     std::string word;
+    std::vector<std::string> outputVector;
     if (file.is_open()) {    
         while (std::getline(file, word)) {
             // testing pigify from header
             std::cout << pigifyWord(word) << std::endl;
             // implement functionality here
+            // need to push each word to vector!
+
         }
     } else {
         std::cerr << "Could not open file!" << std::endl;
     }
+    return outputVector;
 }
 
 void GenerateDictionary(const std::string &fileName) {
