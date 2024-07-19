39305db10cd5945afe9a7871521a1e5f55c381bf
goshag45 <goshagugl44@gmail.com>
2024-07-19 14:21:32 +1000

    completely stupid rudimentary loop - will need to optimise

--- unpigify.cpp
+++ unpigify.cpp
@@ -5,9 +5,15 @@
 
 using std::string;
 
-string unpigifyWord(const string &word) {
-    std::map<string, string> dictionary;
-    // TODO
+string unpigifyWord(const string &word, std::map<string, string> dictionary) {
+    std::map<string, string>::iterator it;
+
+    for (it = dictionary.begin(); it != dictionary.end(); ++it) {
+        if (word == it->first) {
+            return it->second;
+        }
+    }
+    
     return word;
 }
 
