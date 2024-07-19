37349333f0810f191436a013671be9aea280b06b
goshag45 <goshagugl44@gmail.com>
2024-07-19 14:26:02 +1000

    added some capitalisation, handle punctuation in sentence function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <string.h>
-
 #include <algorithm>
 #include <iostream>
 #include <limits>
@@ -76,13 +75,14 @@ string pigifyWord(const string &word) {
     size_t pos = 0;
     string coreWord = word;
     string pigifiedWord;
-    string punctuation = "";
+    // string punctuation = "";
 
+    // is this even needed?
     // check for punctutation
-    while (pos < coreWord.size() && ispunct(coreWord.back())) {
-        punctuation = coreWord.back() + punctuation;
-        coreWord.pop_back();
-    }
+    // while (pos < coreWord.size() && ispunct(coreWord.back())) {
+    //     punctuation = coreWord.back() + punctuation;
+    //     coreWord.pop_back();
+    // }
     // check if first ch is uppercase and remember
     bool hasUpper = std::isupper(coreWord[0]);
 
@@ -109,7 +109,8 @@ string pigifyWord(const string &word) {
         pigifiedWord[0] = (char)std::toupper(pigifiedWord[0]);
     }
 
-    return pigifiedWord + punctuation;
+    //return pigifiedWord + punctuation;
+    return pigifiedWord;
 }
 
 string pigifySentence(string &InputSentence) {
--- unpigify.cpp
+++ unpigify.cpp
@@ -3,17 +3,32 @@
 #include <iostream>
 #include <map>
 
+#include <algorithm>
+#include <limits>
+#include <sstream>
+#include <vector>
+
 using std::string;
 
 string unpigifyWord(const string &word, std::map<string, string> dictionary) {
     std::map<string, string>::iterator it;
+    string pigifiedWord = word;
+    
+    bool hasUpper = std::isupper(pigifiedWord[0]);
+    // from pigify in main
+    std::transform(pigifiedWord.begin(), pigifiedWord.end(), pigifiedWord.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
 
     for (it = dictionary.begin(); it != dictionary.end(); ++it) {
         if (word == it->first) {
             return it->second;
         }
     }
-    
+
+    if (hasUpper) {
+        pigifiedWord[0] = (char)std::toupper(pigifiedWord[0]);
+    }
+
     return word;
 }
 
